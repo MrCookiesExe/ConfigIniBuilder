fe8e11923eda0aa6c925edf5fc6dcf4e2ad6afd8
MrCookiesExe <91439949+MrCookiesExe@users.noreply.github.com>
2022-04-24 00:18:59 +0100

    Add files via upload

---
+++ config ini/EditorV1/build.js
@@ -0,0 +1,318 @@
+function SetWidth() {
+    var b = document.getElementsByTagName("body")[0]
+    for (e of document.getElementsByClassName("tooltip")) {
+        e.style.width = Math.floor((b.scrollWidth - 46) * .8) + "px"
+    }
+}
+
+function generateId() {
+    return Math.random().toString(36).substr(2);
+}
+
+function addRow(table) {
+    var output = document.getElementById(table)
+    var key = document.getElementById(table + "K")
+    if (key.value == "" || typeof key == typeof undefined)
+        return;
+    var left = document.getElementById(table + "L")
+    var right = document.getElementById(table + "R")
+    if (typeof left == typeof undefined || typeof right == typeof undefined || left.value == "" || right.value == "")
+        return;
+    var ident = generateId()
+    output.innerHTML += `<div style="display: table-row;" id="${table}${ident}">
+    <div style="display: table-cell; width: 30%;"><b>${key.value}</b></div>
+    <div style="display: table-cell; width: 30%;">${left.value}</div>
+    <div style="display: table-cell; width: 30%;">${right.value}</div>
+    <div style="display: table-cell; width: 10%;">
+        <b style="cursor: pointer;" onclick="RemoveFromTable('${table}${ident}')">&times;</b>
+    </div></div>`
+    key.value = ""
+}
+
+function addRowSmart(table) {
+    var output = document.getElementById(table)
+    var key = document.getElementById(table + "K")
+    if (key.value == "" || typeof key == typeof undefined)
+        return;
+
+    var d = document.getElementById(table + "D")
+    var c = document.getElementById(table + "C")
+    if (typeof d == typeof undefined || typeof c == typeof undefined || d.value == "" || c.value == "")
+        return;
+    var ident = generateId()
+    output.innerHTML += `<div style="display: table-row;" id="${table}${ident}">
+    <div style="display: table-cell; width: 30%;"><b>${key.value}</b></div>
+    <div style="display: table-cell; width: 30%;">${d.value}</div>
+    <div style="display: table-cell; width: 30%;">${c.value.replace("\n", "<br>")}</div>
+    <div style="display: table-cell; width: 10%;">
+        <b style="cursor: pointer;" onclick="RemoveFromTable('${table}${ident}')">&times;</b>
+    </div></div>`
+    key.value = ""
+    d.value = ""
+    c.value = ""
+}
+
+function addRowTime(table) {
+    var output = document.getElementById(table)
+    var key = document.getElementById(table + "K")
+    if (key.value == "" || typeof key == typeof undefined)
+        return;
+
+    var t = document.getElementById(table + "T")
+    if (typeof t == typeof undefined || t.value == "")
+        return;
+    var ident = generateId()
+    var at = ""
+    var x = { A: "All", S: "Start", M: "Middle", F: "Final" }
+    for (el in x) {
+        var temp = document.getElementById(table + el)
+        if (temp.checked) {
+            at += `${x[el]}<br>`
+        }
+        temp.checked = false
+    }
+    output.innerHTML += `<div style="display: table-row;" id="${table}${ident}">
+    <div style="display: table-cell; width: 20%;">${key.value}</div>
+    <div style="display: table-cell; width: 20%;">${document.getElementById(table + "Mv").value}</div>
+    <div style="display: table-cell; width: 20%;">${t.value}</div>
+    <div style="display: table-cell; width: 20%;">${at}</div>
+    <div style="display: table-cell; width: 20%;">
+        <b style="cursor: pointer;" onclick="RemoveFromTable('${table}${ident}')">&times;</b>
+    </div></div>`
+    key.value = ""
+    t.value = ""
+}
+
+function getValuesMulti(base) {
+    var v = []
+    var s = document.getElementById(base + "-S")
+    var m = document.getElementById(base + "-M")
+    var f = document.getElementById(base + "-F")
+    if (typeof s != "undefined") {
+        v.push(s.value)
+    } else {
+        v.push("")
+    }
+    if (typeof m != "undefined") {
+        v.push(m.value)
+    } else {
+        v.push("")
+    }
+    if (typeof f != "undefined") {
+        v.push(f.value)
+    } else {
+        v.push("")
+    }
+    return v
+}
+
+function addRowMult(table) {
+    var output = document.getElementById(table)
+    var key = document.getElementById(table + "K")
+    if (key.value == "" || typeof key == typeof undefined)
+        return;
+    var left = document.getElementById(table + "L-S")
+    if (typeof left == typeof undefined || left.value == "")
+        return;
+    var ident = generateId()
+    var count = 0
+    var l = getValuesMulti(table + "L").map(elem => {
+        switch (count++) {
+            case 0: return "Start: " + elem
+            case 1: return "Middle: " + elem
+            case 2: return "Final: " + elem
+        }
+    })
+    count = 0
+    var r = getValuesMulti(table + "R").map(elem => {
+        switch (count++) {
+            case 0: return "Start: " + elem
+            case 1: return "Middle: " + elem
+            case 2: return "Final: " + elem
+        }
+    })
+
+    output.innerHTML += `<div style="display: table-row;" id="${table}${ident}">
+    <div style="display: table-cell; width: 30%;"><b>${key.value}</b></div>
+    <div style="display: table-cell; width: 30%;">${l.join("<br>")}</div>
+    <div style="display: table-cell; width: 30%;">${r.join("<br>")}</div>
+    <div style="display: table-cell; width: 10%;">
+        <b style="cursor: pointer;" onclick="RemoveFromTable('${table}${ident}')">&times;</b>
+    </div></div>`
+    key.value = ""
+}
+
+function addRowTwo(table) {
+    var output = document.getElementById(table)
+    var key = document.getElementById(table + "K")
+    if (key.value == "" || typeof key == typeof undefined)
+        return;
+    var value = document.getElementById(table + "V")
+    if (typeof value == typeof undefined || value.value == "")
+        return;
+    var ident = generateId()
+    output.innerHTML += `<div style="display: table-row;" id="${table}${ident}">
+    <div style="display: table-cell; width: 30%;"><b>${key.value}</b></div>
+    <div style="display: table-cell; width: 30%;">${value.value}</div>
+    <div style="display: table-cell; width: 10%;">
+        <b style="cursor: pointer;" onclick="RemoveFromTable('${table}${ident}')">&times;</b>
+    </div></div>`
+    key.value = ""
+}
+
+function RemoveFromTable(id) {
+    var el = document.getElementById(id)
+    el.parentNode.removeChild(el)
+}
+
+function ChengeToIds(sec) {
+    switch (sec) {
+        case "Settings": return "settings";
+        case "ConsonantsAuto": return "Smart";
+        case "ConsonantsTime": return "Time";
+        case "Smart": return "ConsonantsAuto";
+        case "Time": return "ConsonantsTime";
+        default: return sec;
+    }
+}
+function ChengeToValues(value) {
+    switch (value) {
+        case "local": case "Local": return "L"
+        case "global": case "Global": return "G"
+        case "plugin": case "Plugin": return "P"
+        case "resources": case "Resources": return "R"
+        default: return value;
+    }
+}
+
+
+document.getElementById('file').onchange = function a() {
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function (progressEvent) {
+        let ini = {}
+        let lines = this.result.split(/\r?\n/)
+        let sec = ""
+        lines.forEach(line => {
+            if (line.startsWith(";") || line.startsWith("#")) return
+            if (line.startsWith("[")) {
+                line = line.slice(1, -1)
+                ini[line] = {}
+                sec = line
+            } else {
+                let data = line.split("=")
+                ini[sec][data.shift()] = data.join("=")
+            }
+        });
+
+        for (sec in ini) {
+            for (let key in ini[sec]) {
+                if (key == "") continue;
+                let c;
+                switch (ChengeToIds(sec)) {
+                    case "settings":
+                        switch (key) {
+                            case "regex":
+                                document.getElementById(`${ChengeToIds(sec)}/${key}`).value = ini[sec][key].split(",").join("\n")
+                                break;
+                            default:
+                                if (document.getElementById(`${ChengeToIds(sec)}/${key}`) == null)
+                                    break;
+                                let data = ini[sec][key].split(",")
+                                document.getElementById(`${ChengeToIds(sec)}/${key}`).value = data[0]
+                                if (data.length > 1) {
+                                    document.getElementById(`${ChengeToIds(sec)}/${key}/where`).value = ChengeToValues(data[1])
+                                }
+                                break;
+                        }
+                        break;
+                    case "Replace": case "ReplacePieces":
+                        document.getElementById(`${ChengeToIds(sec)}-K`).value = key
+                        document.getElementById(`${ChengeToIds(sec)}-V`).value = ini[sec][key]
+                        document.getElementById(`${ChengeToIds(sec)}-Add`).click()
+                        break;
+                    case "Vowels":
+                        document.getElementById(`${ChengeToIds(sec)}-K`).value = key
+                        document.getElementById(`${ChengeToIds(sec)}-L`).value = ini[sec][key].split(",")[0]
+                        document.getElementById(`${ChengeToIds(sec)}-R`).value = ini[sec][key].split(",")[1]
+                        document.getElementById(`${ChengeToIds(sec)}-Add`).click()
+                        break;
+                    case "Consonants":
+                        c = ini[sec][key].split(",")
+                        let clean = [
+                            "Consonants-L-S",
+                            "Consonants-R-S",
+                            "Consonants-L-M",
+                            "Consonants-R-M",
+                            "Consonants-L-F",
+                            "Consonants-R-F"
+                        ]
+                        clean.forEach(cl => {
+                            document.getElementById(cl).value = ""
+                        })
+                        document.getElementById(`${ChengeToIds(sec)}-K`).value = key
+                        for (let i = 0; i < c.length; i++) {
+                            document.getElementById(clean[i]).value = c[i]
+                        }
+                        document.getElementById(`${ChengeToIds(sec)}-Add`).click()
+                        break;
+                    case "Smart":
+                        document.getElementById(`${ChengeToIds(sec)}-K`).value = key
+                        c = ini[sec][key].split(",")
+                        document.getElementById(`${ChengeToIds(sec)}-D`).value = c.shift()
+                        document.getElementById(`${ChengeToIds(sec)}-C`).value = c.join("\n")
+                        document.getElementById(`${ChengeToIds(sec)}-Add`).click()
+                        break;
+                    case "Time":
+                        if (key == "IsMs") {
+                            document.getElementById(`ConsonantsTime/IsMs`).value = ini[sec][key];
+                            break;
+                        }
+                        document.getElementById(`${ChengeToIds(sec)}-K`).value = key
+                        c = ini[sec][key].split(",")
+                        document.getElementById(`${ChengeToIds(sec)}-Mv`).value = c.shift()
+                        document.getElementById(`${ChengeToIds(sec)}-T`).value = c.shift()
+
+                        c = c[0]
+
+                        let at = ["A", "S", "M", "F"]
+
+                        at.forEach(a => {
+                            if (c.includes(a)) {
+                                document.getElementById(`${ChengeToIds(sec)}-${a}`).checked = true
+                            } else {
+                                document.getElementById(`${ChengeToIds(sec)}-${a}`).checked = false
+                            }
+                        })
+
+                        document.getElementById(`${ChengeToIds(sec)}-Add`).click()
+                        break;
+                    default:
+                        let elem = document.getElementById(`${ChengeToIds(sec)}/${key}`)
+                        if (elem != null)
+                            elem.value = ini[sec][key];
+                        break;
+                }
+            }
+        }
+    };
+    reader.readAsText(file);
+};
+function openConf() {
+    document.getElementById("file").click()
+}
+function saveConf() {
+
+    console.log(out)
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(out));
+    element.setAttribute('download', "config.ini");
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
